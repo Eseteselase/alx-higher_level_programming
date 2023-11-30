91901eeb936dc4143add1d348d8995d63f0189f0
Eseteselase <esete.s.berhanu@gmail.com>
2023-11-30 18:03:42 +0300

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(size - 1))
     for i in range(1, size):
-        print("{}: {}".fofrmat(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
