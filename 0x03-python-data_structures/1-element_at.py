a5f3c0a17ebebcdc409fd2e210a2ac3c14ba29dc
Eseteselase <esete.s.berhanu@gmail.com>
2023-12-02 10:54:10 +0300

    return_none

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
-        exit
+        return
     if idx < len(my_list) and idx > 0:
         return (my_list[idx])
