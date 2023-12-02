ca3941a813a9856511da82463f12bcf5ad6a7ca6
Eseteselase <esete.s.berhanu@gmail.com>
2023-12-02 19:06:13 +0300

    del out of range

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,6 +2,8 @@
 def delete_at(my_list=[], idx=0):
     if my_list == []:
         return None
+    if idx < 0 or idx > len(my_list):
+        return my_list
     else:
         del (my_list[idx])
         for idx in range(idx+1, len(my_list)):
