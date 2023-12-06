6fdc7d696a9c4b28b4f01e679b6b0fcf2635d586
Eseteselase <esete.s.berhanu@gmail.com>
2023-12-06 23:20:50 +0300

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in range(0,len(my_list)):
+        if my_list[i] == search:
+            new_element = replace
+        else:
+            new_element = my_list[i]
+        new_list.append(new_element)
+    return new_list
